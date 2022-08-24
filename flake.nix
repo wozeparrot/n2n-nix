0529f5dd41db1d981e0a0635f5fbe5ee0c044d1a
Woze Parrot <wozeparrot@gmail.com>
2022-08-23 20:29:10 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "n2n packaged for nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    n2n = {
+      url = "github:ntop/n2n/dev";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          packages.n2n = pkgs.stdenv.mkDerivation {
+            name = "n2n";
+            version = inputs.n2n.shortRev;
+            src = inputs.n2n;
+
+            nativeBuildInputs = with pkgs; [
+              autoreconfHook
+              pkg-config
+            ];
+            buildInputs = with pkgs; [ ];
+
+            postPatch = ''
+              patchShebangs autogen.sh
+            '';
+
+            preAutoreconf = ''
+              ./autogen.sh
+            '';
+
+            PREFIX = placeholder "out";
+          };
+        }
+      );
+}
